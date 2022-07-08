83aae2fa685ebf837820f3dd07fe3ca994574cfd
baro-baro <lowlie98@naver.com>
2022-07-08 18:17:37 +0900

    feat: textinput 영역 활성화, 버튼에 임시 기능 부착 기존의 div를 textarea로 변경하고 스타일을 수정했다. 자바스크립트로 메뉴 버튼을 지정했고 임시 기능을 붙였다.
    error: webkit-scrollbar-thumb 에 지정한 스타일이 적용되지 않는다.

---
+++ main.js
@@ -0,0 +1,16 @@
+const newFolderButton = document.getElementById("newFolderButton");
+const settingButton = document.getElementById("settingButton");
+const selectButton = document.getElementById("selectButton");
+const trashBinButton = document.getElementById("trashBinButton");
+const editNoteButton = document.getElementById("editNoteButton");
+const fontSettingButton = document.getElementById("fontSettingButton");
+const imageButton = document.getElementById("imageButton");
+const shareButton = document.getElementById("shareButton");
+const searchButton = document.getElementById("searchButton");
+
+const folderArea = document.getElementsByClassName("folderArea");
+const listArea = document.getElementsByClassName("listArea");
+const textinput = document.getElementsByClassName("textinput");
+
+newFolderButton.addEventListener("click", () => {alert("New Folder")});
+
