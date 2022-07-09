6c6da0ae3dbe907dc6189d13ff4b1cbd6f695490
baro-baro <lowlie98@naver.com>
2022-07-09 17:43:18 +0900

    feat: menu 디자인 완성, 다크모드 메뉴 버튼의 디자인을 마쳤고, 평소에는 보이지 않다가 버튼을 누르면 메뉴가 나오도록 만들었고 다크모드 색상을 정하고 설정의 다크모드 버튼을 누르면 색상이 바뀌도록 만들었다.

--- js/main.js
+++ js/main.js
@@ -8,14 +8,57 @@ const imageButton = document.getElementById("imageButton");
 const shareButton = document.getElementById("shareButton");
 const searchButton = document.getElementById("searchButton");
 
+
+const darkThemeButton = document.getElementById("darkThemeButton");
+
 const folderArea = document.getElementsByClassName("folderArea");
 const listArea = document.getElementsByClassName("listArea");
 const textArea = document.getElementsByClassName("textArea");
 
 newFolderButton.addEventListener("click", () => {alert("New Folder")});
 
+//focus on the text edit erea
 editNoteButton.addEventListener('click', () => { 
 	document.getElementById('textInput').focus();
 });
 
+//pop-up menu
+settingButton.addEventListener('click', () => {
+	const settingContent = document.querySelector('.settingContent');
+	if ( settingContent.style.display === "none" ){
+		settingContent.style.display = "block";
+	} else {
+		settingContent.style.display = "none";
+	}
+});
 
+fontSettingButton.addEventListener('click', () => {
+	const fontContent = document.querySelector('.fontContent');
+	if ( fontContent.style.display === "none" ){
+		fontContent.style.display = "block";
+	} else {
+		fontContent.style.display = "none";
+	}
+});
+
+shareButton.addEventListener('click', () => {
+	const shareContent = document.querySelector('.shareContent');
+	if ( shareContent.style.display === "none" ){
+		shareContent.style.display = "block";
+	} else {
+		shareContent.style.display = "none";
+	}
+});
+
+//dark mode change
+document.addEventListener("DOMContentLoaded", function(event) {
+	document.documentElement.setAttribute("data-theme", "light");
+	
+	darkThemeButton.onclick = function() {
+		let currentTheme = document.documentElement.getAttribute("data-theme");
+
+		let switchTheme = currentTheme === "dark" ? "light" : "dark"
+
+		document.documentElement.setAttribute("data-theme", switchTheme);
+	}
+});
