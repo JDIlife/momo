e01f9fb6f97fbc6961c018076deafcbb05558fbb
baro-baro <lowlie98@naver.com>
2022-07-13 18:20:34 +0900

    feat: crud 작업중 error: div contenteditable=true 안에 작성한 내용이 data객체로 옮겨지지 않는다. text변수에도 제대로 값이 전달되지 않는 것 같다.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// menu buttons
 const newFolderButton = document.getElementById("newFolderButton");
 const settingButton = document.getElementById("settingButton");
 const selectButton = document.getElementById("selectButton");
@@ -8,21 +9,58 @@ const imageButton = document.getElementById("imageButton");
 const shareButton = document.getElementById("shareButton");
 const searchButton = document.getElementById("searchButton");
 
-
-const darkThemeButton = document.getElementById("darkThemeButton");
-
+// three big area
 const folderArea = document.getElementsByClassName("folderArea");
 const listArea = document.getElementsByClassName("listArea");
-const textArea = document.getElementsByClassName("textArea");
+const textArea = document.getElementById("textArea");
 
-newFolderButton.addEventListener("click", () => {alert("New Folder")});
 
-//focus on the text edit erea
+// detail select
+const folder = document.querySelector('.folderItem');
+const darkThemeButton = document.getElementById("darkThemeButton");
+const autoDateTime = document.getElementById("autoDateTime");
+
+// CRUD select
+const textBox = document.getElementById('textBox');
+const submitButton = document.getElementById('submitButton');
+
+let contenteditable = document.querySelector('[contenteditable = true]');
+let text = contenteditable.innerText;
+
+// form validation
+
+submitButton.addEventListener('click', (e) => {
+	e.preventDefault();
+	console.log("button clicked");
+
+	formValidation();
+});
+
+let formValidation = () => {
+	if (text === ""){
+		autoDateTime.innerHTML = "post cannot be blank";
+		console.log("failure");
+	} else {
+		acceptData();
+	}
+};
+
+
+// accept data
+
+let data = {};
+
+let acceptData = () => {
+	data["text"] = text;
+	console.log(data);
+};
+
+// focus on the text edit erea
 editNoteButton.addEventListener('click', () => { 
 	document.getElementById('textBox').focus();
 });
 
-//pop-up menu
+// pop-up menu
 settingButton.addEventListener('click', () => {
 	const settingContent = document.querySelector('.settingContent');
 	if ( settingContent.style.display === "none" ){
@@ -50,7 +88,7 @@ shareButton.addEventListener('click', () => {
 	}
 });
 
-//dark mode change
+// dark mode change
 document.addEventListener("DOMContentLoaded", function(event) {
 	document.documentElement.setAttribute("data-theme", "light");
 	
@@ -62,3 +100,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		document.documentElement.setAttribute("data-theme", switchTheme);
 	}
 });
+
+// create new folder
+
+newFolderButton.addEventListener('click', () => {
+	let div = document.createElement('div');
+
+	div.innerHTML = document.querySelector('.folderItem').innerHTML;
+	div.classList.add("folderItem");
+	document.getElementById('folderItems').appendChild(div);
+});
+
+// select folder
+
+
+//create new note
+
---
+++ js/storage.js
@@ -0,0 +1,39 @@
+myStorage = window.localStorage;
+
+// create folder
+
+
+
+
+
+
+
+
+// select folder
+
+
+
+
+
+
+
+// select listItem
+
+
+
+
+
+// create new note
+
+
+
+// edit text
+
+
+// instant delete listItem
+
+
+// real delete lisItem
+
+
+// move to other folder
